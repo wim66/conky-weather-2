6e85849506364ac42e0b3096793b1fc08fe168be
Willem <20155866+wim66@users.noreply.github.com>
2024-06-06 14:01:31 +0200

    added icons, fixed °C °F difference

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # conky-weather-2
 
+<p align="center"> <img src="https://github.com/wim66/conky-weather-lua/blob/main/Change-settings.png" alt="image"></p>
+
 <p align="center"> <img src="https://github.com/wim66/conky-weather-2/blob/main/preview.png" alt="image"></p>
--- resources/get_weather.sh
+++ resources/get_weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# weather-conky get_weather.sh
+# get_weather.sh
 # by @wim66
 # june 2 2024
 
@@ -8,20 +8,20 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 WEATHER_DATA="$SCRIPT_DIR/weather_data"
 CACHE_DIR="$SCRIPT_DIR/cache"
-ICON_DIR="$SCRIPT_DIR/weather-icons/light/spils-icons"
+ICON_DIR="$SCRIPT_DIR/weather-icons/dark/SagiSan"
 WEATHER_DATA="$SCRIPT_DIR/weather_data"
 
 API_KEY="$OWM_API_KEY" # put your OpenWeatherMap api here https://openweathermap.org/
 CITY_ID="2759794"      # find your city id in the url box https://openweathermap.org/city/2759794
-UNITS="metric" # of 'imperial' voor Fahrenheit
-LANG="nl" # Nederlands
+UNITS="metric" # 'metric' for Celsius or 'imperial' for Fahrenheit
+LANG="nl"
 #URL="http://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&units=metric"
 WEATHER_RESPONSE=$(curl -s "http://api.openweathermap.org/data/2.5/weather?id=$CITY_ID&appid=$API_KEY&units=$UNITS&lang=$LANG")
 
 # Create cache directory if it does not exist
 mkdir -p $CACHE_DIR
 
-# Parse de JSON respons en log elke stap
+# Parse JSON response
 CITY=$(echo $WEATHER_RESPONSE | jq -r .name)
 WEATHER_ICON=$(echo $WEATHER_RESPONSE | jq -r '.weather[0].icon')
 WEATHER_DESC=$(echo $WEATHER_RESPONSE | jq -r '.weather[0].description')
@@ -29,6 +29,12 @@ TEMP=$(echo $WEATHER_RESPONSE | jq -r '.main.temp')
 HUMIDITY=$(echo $WEATHER_RESPONSE | jq -r '.main.humidity') 
 WIND_SPEED=$(echo $WEATHER_RESPONSE | jq -r '.wind.speed')
 
+TEMP=${TEMP%.*}
+if [ "$UNITS" = "metric" ]; then
+    TEMP="${TEMP}°C"
+elif [ "$UNITS" = "imperial" ]; then
+    TEMP="${TEMP}°F"
+fi
 # Copy the icon to the cache directory
 cp ${ICON_DIR}/${WEATHER_ICON}.png ${CACHE_DIR}/weathericon.png
 
